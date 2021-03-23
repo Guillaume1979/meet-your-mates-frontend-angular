9bf05227cba4f3947e985b6c4be92f73dd00baa3
Guillaume1979 <guibetguillaume@gmail.com>
2021-03-23 11:34:43 +0100

    Redefining template composition and adding sass variables

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { RouterModule } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, FooterComponent, NavbarComponent],
   imports: [CommonModule, RouterModule],
-  exports: [HeaderComponent, FooterComponent],
+    exports: [HeaderComponent, FooterComponent, NavbarComponent],
 })
 export class LayoutModule {}
