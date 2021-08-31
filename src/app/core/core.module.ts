966dffcd442c2ba352b07a6831f68fb330fa473c
Guillaume1979 <guibetguillaume@gmail.com>
2021-08-31 16:47:59 +0200

    Adding error message when the injection of the core module happens more than once

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutModule } from './layout/layout.module';
@@ -26,4 +26,12 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     FontAwesomeModule,
   ],
 })
-export class CoreModule {}
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() coreModule: CoreModule) {
+    if (coreModule) {
+      const msg = 'CoreModule is already loaded';
+      console.log(msg);
+      throw new Error(msg);
+    }
+  }
+}
