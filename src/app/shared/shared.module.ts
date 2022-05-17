dc8cb580834733716dc061ec2051cfda552af5c0
Guillaume1979 <guibetguillaume@gmail.com>
2022-05-17 17:03:10 +0200

    Chore: export avatar pipe in shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,11 +3,18 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AvatarPipe } from './pipe/avatar.pipe';
 
 @NgModule({
-  declarations: [],
+  declarations: [AvatarPipe],
   imports: [],
   providers: [],
-  exports: [CommonModule, ReactiveFormsModule, RouterModule, FontAwesomeModule],
+  exports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RouterModule,
+    FontAwesomeModule,
+    AvatarPipe,
+  ],
 })
 export class SharedModule {}
